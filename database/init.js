388f42990c01312c53e0d9bde838e899ed8b83de
AmarGelila <merogelila@gmail.com>
2026-06-07 19:33:16 +0300

    Unignore pnpm-lock.yaml

--- database/init.js
+++ database/init.js
@@ -54,18 +54,14 @@ const SQL = `
 `;
 
 async function main() {
-  console.log("seeding...");
-  const client = new Client({
-    user: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    host: process.env.DATABASE_HOST,
-    port: process.env.DATABASE_PORT,
-    database: process.env.DATABASE_NAME,
-  });
-  await client.connect();
-  await client.query(SQL);
-  await client.end();
-  console.log("done");
+	console.log("seeding...");
+	const client = new Client({
+		connectionString: process.env.DATABASE_URI,
+	});
+	await client.connect();
+	await client.query(SQL);
+	await client.end();
+	console.log("done");
 }
 
 main();
