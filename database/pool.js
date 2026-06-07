a98b5f40a2e40ed687d7f845eab226f188c53e99
AmarGelila <merogelila@gmail.com>
2026-06-07 19:27:53 +0300

    Add Database URI Enviroment Variable

--- database/pool.js
+++ database/pool.js
@@ -1,12 +1,9 @@
+import "dotenv/config";
 import { Pool } from "pg";
 
 const pool = new Pool({
-  user: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD,
-  host: process.env.DATABASE_HOST,
-  port: process.env.DATABASE_PORT || 5432,
-  database: process.env.DATABASE_NAME,
-  ssl: { rejectUnauthorized: false },
+	connectionString: process.env.DATABASE_URI,
+	ssl: { rejectUnauthorized: false },
 });
 
 export default pool;
